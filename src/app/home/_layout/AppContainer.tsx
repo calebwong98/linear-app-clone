6a16c00f347f90024e17c25d857ff696ea667b48
Caleb Wong <jialewong98@gmail.com>
2026-04-07 17:50:44 +0800

    feat: global appcontainer style and header

---
+++ src/app/home/_layout/AppContainer.tsx
@@ -0,0 +1,20 @@
+import { cn } from "@/lib/utils";
+
+export default function AppContainer({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "max-w-(--homepage-max-width) mx-auto px-6 sm:px-9 lg:px-12 xl:px-20 py-5",
+        className,
+      )}
+    >
+      {children}
+    </div>
+  );
+}
--- src/app/home/_layout/Header.tsx
+++ src/app/home/_layout/Header.tsx
@@ -1,16 +1,25 @@
 "use server";
 
 import { Logo } from "@/components/shared/Logo";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import AppContainer from "./AppContainer";
 
 export default async function Header() {
   return (
     <header className="w-full z-50 fixed border-b border-border-primary backdrop-blur-[20px]">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+      <AppContainer className="flex justify-between items-center">
         <Link href="/" className="flex items-center gap-2">
           <Logo />
         </Link>
-      </div>
+
+        <div className="flex items-center gap-2">
+          <Button>Log in</Button>
+          <Button variant={"secondary"}>Sign up</Button>
+        </div>
+
+        {/* hamburger menu */}
+      </AppContainer>
     </header>
   );
 }
